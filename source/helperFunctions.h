e2856ff93caf9bb516f86380a18c9bba007dce98
dustinlapierre <dustinlapierre@yahoo.com>
2016-10-29 15:00:30 -0400

    Added helper function files

---
+++ source/helperFunctions.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+char **parsePath(char * pathname);
+
+#endif
